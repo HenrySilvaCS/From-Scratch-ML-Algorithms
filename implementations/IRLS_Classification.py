1a470cfc771e7eda07be070762b660451f55678d
Henrique Soares Assumpção e Silva <henriquesoares@dcc.ufmg.br>
2020-10-17 17:43:41 -0300

    Update and rename Newton-Raphson Classification.py to IRLS_Classification.py

--- implementations/IRLS_Classification.py
+++ implementations/IRLS_Classification.py
@@ -25,3 +25,17 @@ def irls(X,y,n_iter):
 		z = X @ theta + np.linalg.pinv(weight_matrix(X,theta)) @ (y - probability_vector(X,theta))
 		theta = np.linalg.pinv(X.T @ weight_matrix(X,theta) @ X) @ X.T @ weight_matrix(X,theta) @ z 
 	return theta
+
+
+class IRLS_Classfier:
+	def __init__(self,n_iter):
+		self.n_iter = n_iter
+	def fit(self,x,y):
+		self.x = x
+		self.y = y
+		self.parameters = irls(self.x,self.y,self.n_iter)
+	def predict(self,X):
+		ones = np.ones((np.size(X,0),1))
+		X_copy = np.hstack((ones,X))
+		return X_copy @ self.parameters
+		
